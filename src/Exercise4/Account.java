d529b73b9776b9a1b22e24977579bea7e0c0b01d
idkwhoami <124172865+rithysokha@users.noreply.github.com>
2023-07-24 14:58:08 +0700

    now cannot transfer to own account

--- src/Exercise4/Account.java
+++ src/Exercise4/Account.java
@@ -1,4 +1,5 @@
 package Exercise4;
+
 import java.util.HashMap;
 
 public class Account {
@@ -6,8 +7,6 @@ public class Account {
     private double balance;
     private int accountNumber;
     private int password;
-    
-    
 
     // setter getter for accesing private variable
     public String getName() {
@@ -50,49 +49,71 @@ public class Account {
     }
 
     public HashMap<Integer, Account> accountList = new HashMap<>();
-   
+
     void displayLine() {
         System.out.println("============================================");
     }
+
+    // login method will return true if the password and account number is correct
     boolean logIn(int accountNumber, int password) {
+        displayLine();
         if (accountList.containsKey(accountNumber) && accountList.get(accountNumber).getPassword() == password) {
             System.out.println("Login successful");
             System.out.println("Welcome " + accountList.get(accountNumber).getName());
             return true;
         } else {
-            System.out.println("Login failed");
+            System.out.println("Login failed.");
             return false;
 
         }
     }
-    void displayBalance(int accountNumber){
-        System.out.println("Your balance is: " + accountList.get(accountNumber).getBalance());
+
+    // display balance of user
+    void displayBalance(int accountNumber) {
+        displayLine();
+        System.out.println("Your balance is: " + accountList.get(accountNumber).getBalance() + " $");
     }
-    void deposit (int accountNumer, double ammount){
+
+    // deposit balace for user
+    void deposit(int accountNumer, double ammount) {
+        displayLine();
         accountList.get(accountNumer).setBalance(accountList.get(accountNumer).getBalance() + ammount);
-        System.out.println("Deposit successful");
+        System.out.println("Deposit successful!");
     }
-    void withdraw(int accountNumber, double ammount){
-        if(accountList.get(accountNumber).getBalance() < ammount){
-            System.out.println("Insufficient balance");
-        }else{
+
+    // withdraw method will check banlance before withdraq
+    void withdraw(int accountNumber, double ammount) {
+        displayLine();
+        if (accountList.get(accountNumber).getBalance() < ammount) {
+            System.out.println("Insufficient balance.");
+        } else {
             accountList.get(accountNumber).setBalance(accountList.get(accountNumber).getBalance() - ammount);
-            System.out.println("Withdraw successful");
+            System.out.println("Withdraw successful.");
         }
     }
-    void transfer(int accountNumer, int accountNumberTransfer, double ammount){
-        if(!accountList.containsKey(accountNumberTransfer)){
-            System.out.println("Account number not found");
-            return;
-        }
-        if(accountList.get(accountNumer).getBalance() < ammount){
-            System.out.println("Insufficient balance");
-        }else{
-            accountList.get(accountNumer).setBalance(accountList.get(accountNumer).getBalance() - ammount);
-            accountList.get(accountNumberTransfer).setBalance(accountList.get(accountNumberTransfer).getBalance() + ammount);
-            System.out.println("Transfer successful");
+
+    // transfer method will check balance and account validility before work
+    void transfer(int accountNumber, int accountNumberTransfer, double ammount) {
+        displayLine();
+        if (accountNumber != accountNumberTransfer) {
+
+            if (!accountList.containsKey(accountNumberTransfer)) {
+                System.out.println("Account number not found.");
+                return;
+            }
+            if (accountList.get(accountNumber).getBalance() < ammount) {
+                System.out.println("Insufficient balance.");
+            } else {
+                accountList.get(accountNumber).setBalance(accountList.get(accountNumber).getBalance() - ammount);
+                accountList.get(accountNumberTransfer)
+                        .setBalance(accountList.get(accountNumberTransfer).getBalance() + ammount);
+                System.out.println("Transfer successful.");
+            }
+        } else {
+            System.out.println("You can't transfer to your own account.");
         }
     }
+
     void displayMenu() {
         displayLine();
         System.out.println("Please select an option: ");
@@ -102,5 +123,5 @@ public class Account {
         System.out.println("D. Transfer");
         System.out.println("E. Exit");
     }
-    
+
 }
