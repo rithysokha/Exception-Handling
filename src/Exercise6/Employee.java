dc9c07dd3f977ff67c0d8c99f2de0c187ba9f893
idkwhoami <124172865+rithysokha@users.noreply.github.com>
2023-07-27 21:22:07 +0700

    fix exercise6

--- src/Exercise6/Employee.java
+++ src/Exercise6/Employee.java
@@ -1,114 +1,241 @@
-//group8
 package Exercise6;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.*;
 import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Employee {
+
     private int id;
     private String name;
     private String gender;
-    private double salary;
+    private String salary;
+
+    public Employee() {
+    }
+    public Employee(int id, String name, String gender, String salary) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{id=" + id + ", name='" + name + "', gender='" + gender + "', salary='" + salary + "'}";
+    }
+
+//    Ask user to input new employee
+    public void readEmployee(){
+        System.out.println("Add a new employee: ");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter id: ");
+        id = input.nextInt();
+        System.out.print("Enter name: ");
+        name = input.next();
+        System.out.print("Enter gender: ");
+        gender = input.next();
+        System.out.print("Enter salary: ");
+        salary = input.next();
+    }
+
+//    Save employee data into txt file
+    public void addEmployee() {
+        Map<Integer, Employee> employee = new HashMap<>();
+        if(!displayEmployee().isEmpty()){
+            employee.putAll(displayEmployee());
+        }
+        Employee em = new Employee(id, name, gender, salary);
+        employee.put(id, em);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("src\\Exercise6\\employee.txt"))){
+            // Write each employee in the HashMap to the file
+            for (Map.Entry<Integer, Employee> entry : employee.entrySet()) {
+                writer.write(entry.getKey() + ":" + entry.getValue().toString() + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+//    Display employees from txt file
+    public Map<Integer, Employee> displayEmployee() {
+        Map<Integer, Employee> hashMap = new HashMap<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("src\\Exercise6\\employee.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(":");
+                if (parts.length == 2) {
+                    int id = Integer.parseInt(parts[0]);
+                    String employeeData = parts[1].trim();
+                    Employee employee = createEmployeeFromString(employeeData);
+                    hashMap.put(id, employee);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return hashMap;
+    }
+
+//    Update data employees after remove id
+    public void updateEmployee(Map<Integer, Employee> employee) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("src\\Exercise6\\employee.txt"))){
+            // Write each employee in the HashMap to the file
+            for (Map.Entry<Integer, Employee> entry : employee.entrySet()) {
+                writer.write(entry.getKey() + ":" + entry.getValue().toString() + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+//    Remove employee data by id
+    public void deleteEmployee(int employeeId) {
+        Map<Integer, Employee> hashMap = new HashMap<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("src\\Exercise6\\employee.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(":");
+                if (parts.length == 2) {
+                    int id = Integer.parseInt(parts[0]);
+                    String employeeData = parts[1].trim();
+                    Employee employee = createEmployeeFromString(employeeData);
+                        hashMap.put(id, employee);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        hashMap.remove(employeeId);
+
+        updateEmployee(hashMap);
+
+    }
+
+    // Extract the data from txt file and return Employee object
+    private static Employee createEmployeeFromString(String employeeData) {
+        String[] dataParts = employeeData.split(", ");
+        if (dataParts.length == 4) {
+            int id = Integer.parseInt(dataParts[0].substring(12));
+            String name = dataParts[1].substring(6);
+            name = name.replace("'", "");
+            String gender = dataParts[2].replace("gender='", "");
+            gender = gender.replace("'", "");
+            String salary = dataParts[3].replace("salary='", "");
+            salary = salary.replace("'}", "");
+            return new Employee(id, name, gender, salary);
+        }
+        return null;
+    }
 
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getGender() {
         return gender;
     }
 
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public double getSalary() {
+    public String getSalary() {
         return salary;
     }
 
-    public void setSalary(double salary) {
-        this.salary = salary;
+    public void displayLine(){
+        System.out.println("----------------------------------------------------");
     }
+    public static void main(String[] args) {
+        Employee employee = new Employee();
 
-    public Employee(String name, String gender, double salary) {
-        this.name = name;
-        this.gender = gender;
-        this.salary = salary;
-    }
+        boolean exit = false;
+        while (!exit){
+            Scanner input = new Scanner(System.in);
+            System.out.println();
+            System.out.println(" -> Welcome to Employee Management <-");
+            System.out.println("  a. Add a new employee");
+            System.out.println("  b. Delete employee by id");
+            System.out.println("  c. Search employee by id");
+            System.out.println("  d. Display all employees");
+            System.out.println("  e. Exit the program");
+            System.out.print("    make a Choice : ");
+            String choice = input.nextLine();
+            System.out.println();
 
-    public HashMap<Integer, Employee> employeeList = new HashMap<>();
-
-    void readEmployee() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Input your ID: ");
-        id = input.nextInt();
-        input.nextLine();
-        System.out.println("Input your Name: ");
-        name = input.nextLine();
-        System.out.println("Input your gender: ");
-        gender = input.nextLine();
-        System.out.println("Input your salary: ");
-        salary = input.nextDouble();
-        input.close();
-    }
-
-    void addEmployee() {
-        File file = new File("src\\Exercise6\\employee.txt");
-        try {
-            // Creates a reader that is linked with the myFile.txt
-            FileReader reader = new FileReader("src\\Exercise6\\employee.txt");
-            BufferedReader bufferedReader = new BufferedReader(reader);
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                String[] keyValuePair = line.split(", ");
-                String employeeId = keyValuePair[0];
-                String nameString = keyValuePair[1];
-                String genderString = keyValuePair[2];
-                String salaryString = keyValuePair[3];
-                Integer employeeIDInteger = Integer.parseInt(employeeId);
-                double salayDouble = Integer.parseInt(salaryString);
-                // Add the key-value pair to the HashMap.
-                employeeList.put(employeeIDInteger, new Employee(nameString, genderString, salayDouble));
+            switch (choice) {
+                case "a" -> {
+                    employee.displayLine();
+                    employee.readEmployee();
+                    employee.displayLine();
+                    employee.addEmployee();
+                }
+                case "b" -> {
+                    Scanner sc = new Scanner(System.in);
+                    employee.displayLine();
+                    try{
+                        System.out.print("Enter ID to remove employee: ");
+                         int id = sc.nextInt();
+                        Map<Integer, Employee> removeEmployee = employee.displayEmployee();
+                        if (removeEmployee.containsKey(id)) {
+                            employee.deleteEmployee(id);
+                            System.out.println("Id removed successfully!");
+                        } else {
+                            System.out.println("Id not found...");
+                        }
+                    }catch (InputMismatchException e){
+                        System.out.println("Please Enter a number...");
+                    }
+                    employee.displayLine();
+                }
+                case "c" -> {
+                    Scanner scanner = new Scanner(System.in);
+                    employee.displayLine();
+                    try {
+                        System.out.print("Enter ID to search employee: ");
+                        int searchId = scanner.nextInt();
+                        Map<Integer, Employee> searchEmployee = employee.displayEmployee();
+                        if (searchEmployee.containsKey(searchId)) {
+                            Employee em = searchEmployee.get(searchId);
+                            System.out.printf("| %-10s | %-10s | %-10s | %-10s|\n", "ID", "Name", "Gender", "Salary");
+                            System.out.printf("| %-10d | %-10s | %-10s | %-10s|%n", em.getId(), em.getName(), em.getGender(), em.getSalary());
+                        } else {
+                            System.out.println("ID not found...!");
+                        }
+                    }catch (InputMismatchException e){
+                        System.out.println("Please Enter a number...");
+                    }
+                    employee.displayLine();
+                }
+                case "d" -> {
+                    employee.displayLine();
+                    Map<Integer, Employee> getEmployee = employee.displayEmployee();
+                    System.out.println("Display all employees information: \n");
+                    System.out.printf("| %-10s | %-10s | %-10s | %-10s|\n", "ID", "Name", "Gender", "Salary");
+                    for (Map.Entry<Integer, Employee> em : getEmployee.entrySet()) {
+                        Employee employee1 = getEmployee.get(em.getKey());
+                        System.out.printf("| %-10d | %-10s | %-10s | %-10s|%n", employee1.getId(), employee1.getName(), employee1.getGender(), employee1.getSalary());
+                    }
+                    employee.displayLine();
+                }
+                case "e" -> {
+                    employee.displayLine();
+                    System.out.println("Exiting the program...");
+                    exit = true;
+                    employee.displayLine();
+                }
+                default -> {
+                    employee.displayLine();
+                    System.out.println("Invalid Input ...");
+                    employee.displayLine();
+                }
             }
-            employeeList.put(id, new Employee(name, gender, salary));
-            // Close the file.
-            bufferedReader.close();
 
-            reader.close();
-        } catch (Exception e) {
-            System.out.println(e);
         }
-        try {
-            // try to create a file
-            FileWriter writer = new FileWriter(file);
-            for (int key : employeeList.keySet()) {
-                writer.write(key + ", " + employeeList.get(key).getName() + ", " +
-                        employeeList.get(key).getGender() + ", " +
-                        employeeList.get(key).getSalary() + "\n");
-            }
-            writer.close();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-
-    void deleteEmployee(int employeeID) {
-        System.out.println();
     }
 }
+
